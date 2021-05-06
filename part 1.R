5af8ac58bfd52f9b2d294afb20ca0c8c44ced6e9
Pj Fitchett <p.fitchett2@newcastle.ac.uk>
2021-05-06 09:48:13 +0100

    Added raven to analysis, ggplot spectrograms, changed #barn to #tawny Added raven_call and removed nes8010 source script to gitignore Added nes8010.R source script

--- part 1.R
+++ part 1.R
@@ -22,23 +22,34 @@ library(warbleR)
 buzzard_call <- query_xc(qword = 'Buteo buteo cnt:"united kingdom" type:call len:5-25', download = FALSE)
 # 50 recordings found
 
-# Barn owl
+# Tawny owl
 tawny_song <- query_xc(qword = 'Strix aluco cnt:"united kingdom" type:song len:5-25', download = FALSE)
 # 32 recordings
+
+# Common raven call
+raven_call <- query_xc(qword = 'Corvus corax cnt:"united kingdom" type:call len:5-25', download = FALSE)
+# 35 recordings
 
 # Show the location of the buzzard
 map_xc(buzzard_call, leaflet.map = TRUE)
 
-# Show the location of the barn owl
+# Show the location of the tawny owl
 map_xc(tawny_song, leaflet.map = TRUE)
+
+# Show the location of the raven
+map_xc(raven_call, leaflet.map = TRUE)
 
 # Create folders in the project to save the songs and calls to 
 dir.create(file.path("buzzard_call"))
 dir.create(file.path("tawny_song"))
+dir.create(file.path("raven_call"))
 
 # Download the .MP3 files into the relevant folders
 query_xc(X = buzzard_call, path="buzzard_call")
-query_xc(X = barn_owl_call, path="tawny_song")
+query_xc(X = tawny_song, path="tawny_song")
+query_xc(X = raven_call, path="raven_call")
+
+
 
 # Rename files to make them uniform ####
 
@@ -62,11 +73,23 @@ for(file in 1:length(old_files)){
 }
 file.rename(old_files, new_files)
 
+# raven
+old_files <- list.files("raven_call", full.names=TRUE)
+new_files <- NULL
+for(file in 1:length(old_files)){
+  curr_file <- str_split(old_files[file], "-")
+  new_name <- str_c(c(curr_file[[1]][1:2], "-call_", curr_file[[1]][3]), collapse="")
+  new_files <- c(new_files, new_name)
+}
+file.rename(old_files, new_files)
+
 # Copy all recordings to a new folder 
 dir.create(file.path("bird_audio"))
 file.copy(from=paste0("buzzard_call/",list.files("buzzard_call")),
           to="bird_audio")
 file.copy(from=paste0("tawny_song/",list.files("tawny_song")),
+          to="bird_audio")
+file.copy(from=paste0("raven_call/",list.files("raven_call")),
           to="bird_audio")
 
 # Convert MP3 to WAV
@@ -74,6 +97,8 @@ mp32wav(path="bird_audio", dest.path="bird_audio")
 unwanted_mp3 <- dir(path="bird_audio", pattern="*.mp3")
 # Remove unwanted MP3 
 file.remove(paste0("bird_audio/", unwanted_mp3))
+
+
 
 # Visualise and analyse the songs and calls ####
 
@@ -85,13 +110,23 @@ buzzard_call_wav
 oscillo(buzzard_call_wav)
 
 # Focus on a small area of the clip
-oscillo(buzz_call_wav, from = 3.59, to = 3.60)
+oscillo(buzzard_call_wav, from = 3.59, to = 3.60)
 
 # Create a spectrogram for the same clip
 SpectrogramSingle(sound.file = "bird_audio/Buteobuteo-call_264996.wav",
                   Colors = "Colors")
 
-# Now repeat for barn owl ####
+# Create a spectrogram with ggplot
+buzz_spec <- ggspectro(buzzard_call_wav, flim=c(0,7)) + # y-axis limits in kHz
+  geom_tile(aes(fill=amplitude)) +
+  scale_fill_gradient2(name="Amplitude\n(dB)\n", limits=c(-60,0),
+                       na.value="transparent",
+                       low="green", mid="yellow", high="red", midpoint = -30)
+buzz_spec
+
+
+
+# Now repeat for tawny owl ####
 tawny_song_wav <- readWave("bird_audio/Strixaluco-song_473154.wav")
 tawny_song_wav
 
@@ -105,6 +140,42 @@ oscillo(tawny_song_wav, from = 1.49, to = 1.50)
 SpectrogramSingle(sound.file = "bird_audio/Strixaluco-song_473154.wav",
                   Colors = "Colors")
 
+# Create a spectrogram with ggplot
+tawny_spec <- ggspectro(tawny_song_wav, flim=c(0,6)) + # y-axis limits in kHz
+  geom_tile(aes(fill=amplitude)) +
+  scale_fill_gradient2(name="Amplitude\n(dB)\n", limits=c(-60,0),
+                       na.value="transparent",
+                       low="green", mid="yellow", high="red", midpoint = -30)
+tawny_spec
+
+
+
+
+# Now repeat for raven ####
+raven_call_wav <- readWave("bird_audio/Corvuscorax-call_94437.wav")
+raven_call_wav
+
+# Create an oscillogram for the full audio clip
+oscillo(raven_call_wav)
+
+# Focus on a small area of the clip
+oscillo(raven_call_wav, from = 3.49, to = 3.50)
+
+# Create a spectrogram for the same clip
+SpectrogramSingle(sound.file = "bird_audio/Corvuscorax-call_94437.wav",
+                  Colors = "Colors")
+
+# Create a spectrogram with ggplot
+raven_spec <- ggspectro(raven_call_wav, flim=c(0,2)) + # y-axis limits in kHz
+  geom_tile(aes(fill=amplitude)) +
+  scale_fill_gradient2(name="Amplitude\n(dB)\n", limits=c(-60,0),
+                       na.value="transparent",
+                       low="green", mid="yellow", high="red", midpoint = -30)
+raven_spec
+
+
+
+
 # Feature extraction using MFCC ####
 
 # Changing max frequency to 7000 - 2000 would be too low for these birds
@@ -116,8 +187,8 @@ dim(bird_mfcc)
 
 bird_pca <- ordi_pca(bird_mfcc[, -1], scale=TRUE)
 summary(bird_pca)
-# PC1 explains 21.48% of the variation, PC2 explains 10.56%
-# Cumulative 1 and 2 is 32.04%
+# PC1 explains 20.29% of the variation, PC2 explains 10.54%
+# Cumulative 1 and 2 is 30.83%
 
 # Extract scores to display
 bird_sco <- ordi_scores(bird_pca, display="sites")
